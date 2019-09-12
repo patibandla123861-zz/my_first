526893b55af6f2471779616e7eb47bb90de09363
patibandla123 <stshpatibandla@gmail.com>
2019-09-12 16:49:17 +0530

    hi

---
+++ check.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+/**float f=0.1;
+
+if(f==0.1f)
+{
+printf("true");
+}
+else
+{
+printf("false");
+}**/
+int x;
+printf("enter the number:");
+scanf("%d",&x);
+if(x%2==1||x%2==-1)
+{
+printf("odd");
+}
+else
+{
+printf("even");
+}
+}
